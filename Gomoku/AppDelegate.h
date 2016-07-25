e22a03f7425574e38a87bfa562cf972f3a765024
Sekorm <Sekorm@HelloYeah.local>
2016-07-25 10:32:22 +0800

    first commit

---
+++ Gomoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gomoku
+//
+//  Created by Sekorm on 16/7/25.
+//  Copyright © 2016年 HY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gomoku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gomoku
+//
+//  Created by Sekorm on 16/7/25.
+//  Copyright © 2016年 HY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
